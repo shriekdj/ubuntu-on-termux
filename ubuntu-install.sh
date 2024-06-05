fb4f7c83c567e144e214bdc2f1ce684c03cc2c9a
Shrikant Dhayje <shrikantdhayaje@gmail.com>
2024-06-05 19:23:02 +0530

    Update ubuntu-install.sh

--- ubuntu-install.sh
+++ ubuntu-install.sh
@@ -1,7 +1,3 @@
 #!/bin/sh
-pkg install apt-get -y
-apt-get update -y
-apt-get upgrade -y
-apt-get install wget -y
-apt-get install proot-distro -y
+pkg install proot-distro -y
 proot-distro install ubuntu
